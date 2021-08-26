1d4a599c194cf29c836d81176ca7d375bb5276da
xsy <1140388501@qq.com>
2021-08-26 15:23:57 +0800

    gitignore

---
+++ src/write/index.js
@@ -0,0 +1,34 @@
+var data = [
+    {
+        id: 'X2121',
+        name: 'xxx',
+        title: 'xxx',
+        detail: [
+            {
+                date: "2020/10/1",
+                money: '8.7',
+                reason: 'xxx',
+                time: '12:09',
+            }
+        ]
+    },
+    {
+        id: 'X2221',
+        name: 'xxx2',
+        title: 'xxx2',
+        detail: [
+            {
+                date: "2020/10/2",
+                money: '8.2',
+                reason: 'xx2',
+                time: '12:02',
+            }
+        ]
+    },
+]
+
+write(data);
+
+function write(){
+    
+}
