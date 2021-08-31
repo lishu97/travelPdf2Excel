e97a705964b956dee98ca90842221b7109f3f39e
xsy <1140388501@qq.com>
2021-08-31 09:33:50 +0800

    说明文档，高德修改适配

--- src/transform/parseTextGaode.js
+++ src/transform/parseTextGaode.js
@@ -4,23 +4,23 @@ const moment = require('moment');
 const getStartYear = (pdfText) => /行程时间：(?:@@)?(\d*)/g.exec(pdfText)[1];
 
 function parseTextGaoDe(pdfText) {
-    const reg = /@@(?:\d{1,2})@@(?:.*?)@@(?:.*?)@@(?:\d*-)(\d*-\d*)\s(\d*:\d*)@@(?:.*?)@@(?:.*?)@@(?:.*?)@@(\d*\.\d*)@@/g
+    const reg = /@@(?:\d{1,2})@@(?:.*?)@@(?:.*?)@@(\d*-\d*-\d*)\s(\d*:\d*)@@(?:.*?)@@(?:.*?)@@(?:.*?)@@(\d*\.\d*)/g
     let result = reg.exec(pdfText); 
     const detailRecords = [];
-    let year = getStartYear(pdfText)
-    let lastDate = result[1];
+    // let year = getStartYear(pdfText)
+    // let lastDate = result[1];
     while(result !== null){
-      const [lastMonth, lastDay] = lastDate.split('-');
-      const [currentMonth, currentDay] = result[1].split('-');
-      if(lastMonth > currentMonth) {
-        year++;
-        lastDate = `${currentMonth}-${currentDay}`;
-      }
-        const date = `${year}-${currentMonth}-${currentDay}`;
+      // const [lastMonth, lastDay] = lastDate.split('-');
+      // const [currentMonth, currentDay] = result[1].split('-');
+      // if(lastMonth > currentMonth) {
+        // year++;
+        // lastDate = `${currentMonth}-${currentDay}`;
+      // }
+        // const date = `${year}-${currentMonth}-${currentDay}`;
+        const date = result[1];
         detailRecords.push({
         reason: '加班至晚上10点后，无公交',
         money: `${+result[3]}`,
-
         moment: moment(`${date} ${result[2]}`, 'YYYY-MM-DD HH:mm'),
       })
       result = reg.exec(pdfText);  
